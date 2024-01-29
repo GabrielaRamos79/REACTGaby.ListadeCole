dda730bde312c5055a9f3c5e5c0a6dc308c5ea36
GabrielaRamos79 <gabyramos@me.com>
2024-01-29 21:30:35 +0100

    cambios

--- REACTGaby/src/main.jsx
+++ REACTGaby/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import ReactGaby from './components/ReactGaby.jsx';
 import './reactGaby.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'copiaprueba.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
