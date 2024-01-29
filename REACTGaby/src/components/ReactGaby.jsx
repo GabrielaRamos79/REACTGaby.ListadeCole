783221a3b47179fb2a7a34ee7ff226388c0b19ce
GabrielaRamos79 <gabyramos@me.com>
2024-01-29 18:08:40 +0100

    cambios

--- REACTGaby/src/components/ReactGaby.jsx
+++ REACTGaby/src/components/ReactGaby.jsx
@@ -1,5 +1,6 @@
 import './reactGaby.css'
 import React, { useState } from 'react';
+import Nav from 'react-bootstrap/Nav';
 
 const StudentForm = () => {
   const [students, setStudents] = useState([]);
@@ -13,12 +14,30 @@ const StudentForm = () => {
 }
 
 function ReactGaby() {
-  
+  function AlignmentExample(){
   return (
     <>
-      
+      <Nav className="justify-content-end" activeKey="/home">
+        <Nav.Item>
+          <Nav.Link href="/home">Añadir Lista</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1">Editar Lista</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-2">Eliminar Lista</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="disabled" disabled>
+            Disabled
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
     </>
-  )
+  );
 }
+  export default AlignmentExample;
+}
+
 
 export default ReactGaby
